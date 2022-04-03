7d24b348b28c01c060968ec4ccfe1b9f401117e7
jalfaro <jalfaro.pineda@gmail.com>
2022-04-02 19:27:29 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { OperacionesComponent } from './components/operaciones/operaciones.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: OperacionesComponent},
+  {path:'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OperacionesComponent } from './components/operaciones/operaciones.component';
+import { OperacionComponent } from './components/operacion/operacion.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OperacionesComponent,
+    OperacionComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
